88d9f93edf9fb4abd610c9f1b94c966378b30ca8
Nandu Viswanathan <nanduvishwanathan@gmail.com>
2026-02-09 16:50:23 +0530

    chore: Update Vite base path configuration to `/my-valentine/`.

--- valentines-week/vite.config.js
+++ valentines-week/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/valentines-week/', // REPLACE THIS with your repository name if different
+  base: '/my-valentine/', // REPLACE THIS with your repository name if different
 })
